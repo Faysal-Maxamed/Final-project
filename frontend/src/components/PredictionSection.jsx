afaef10d7d368525b0652b80d0495163001430ff
maxamadBashi <maxamedbaashi132@gmail.com>
2025-08-04 17:36:06 +0300

    updated

--- frontend/src/components/PredictionSection.jsx
+++ frontend/src/components/PredictionSection.jsx
@@ -198,7 +198,7 @@ const PredictionSection = () => {
             {/* Age */}
             <div className="flex flex-col w-full">
               <label className="text-xs font-semibold text-gray-700 mb-1">Age</label>
-              <input type="number" name="age" value={formData.age} onChange={handleChange} required min="0"
+              <input type="number" name="age" value={formData.age} onChange={handleChange} required min="1"
                 className="p-3 border border-gray-200 rounded-lg focus:ring-2 focus:ring-indigo-300 focus:border-indigo-400 transition text-base" placeholder="Age" />
             </div>
             {/* Gender */}
@@ -225,13 +225,13 @@ const PredictionSection = () => {
             {/* Procedures */}
             <div className="flex flex-col w-full">
               <label className="text-xs font-semibold text-gray-700 mb-1">Procedures</label>
-              <input type="number" name="num_procedures" value={formData.num_procedures} onChange={handleChange} required min="0"
+              <input type="number" name="num_procedures" value={formData.num_procedures} onChange={handleChange} required min="1"
                 className="p-3 border border-gray-200 rounded-lg focus:ring-2 focus:ring-yellow-200 focus:border-yellow-400 transition text-base" placeholder="#" />
             </div>
             {/* Days */}
             <div className="flex flex-col w-full">
               <label className="text-xs font-semibold text-gray-700 mb-1">Days in Hospital</label>
-              <input type="number" name="days_in_hospital" value={formData.days_in_hospital} onChange={handleChange} required min="0"
+              <input type="number" name="days_in_hospital" value={formData.days_in_hospital} onChange={handleChange} required min="1"
                 className="p-3 border border-gray-200 rounded-lg focus:ring-2 focus:ring-blue-200 focus:border-blue-400 transition text-base" placeholder="Days" />
             </div>
             {/* Submit */}
@@ -281,7 +281,7 @@ const PredictionSection = () => {
                         {result.readmission == 0 ? notReadmissionProbability.toFixed(1) : readmissionProbability.toFixed(1)}%
                       </span>
                       <span className="text-sm text-gray-500">Probability</span>
-                    </div>
+                    </div>
                   </div>
                   <div className={`mt-6 py-2 px-6 rounded-full text-lg font-semibold ${result.readmission === 1 ? "bg-red-100 text-red-700" : "bg-green-100 text-green-700"}`}>
                     {result.readmission === 1 ? "Yes Readmitted" : "Not Readmitted"}
